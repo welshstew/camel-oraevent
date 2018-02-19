91d7530947e9a132047db8c672c456b6c7cafa73
Stuart Winchester <stuart.winchester@gmail.com>
2018-02-19 15:31:54 +0000

    small update, added test properties, removed .travis (will not CI/CD this project)

--- src/test/java/org/apache/camel/component/oraevent/IntegrationTest.java
+++ src/test/java/org/apache/camel/component/oraevent/IntegrationTest.java
@@ -14,49 +14,59 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.camel.oraevent;
+package org.apache.camel.component.oraevent;
 
-
-import org.apache.camel.Exchange;
-import org.apache.camel.Processor;
-import org.apache.camel.ProducerTemplate;
 import org.apache.camel.RoutesBuilder;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.mock.MockEndpoint;
 import org.apache.camel.impl.JndiRegistry;
-import org.apache.camel.main.Main;
 import org.apache.camel.test.junit4.CamelTestSupport;
 import org.apache.commons.dbcp2.BasicDataSource;
 import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.io.File;
+import java.io.FileReader;
+import java.util.Properties;
 
 public class IntegrationTest extends CamelTestSupport {
 
     private BasicDataSource ds;
     JdbcTemplate jdbc;
+    Logger log = LoggerFactory.getLogger(this.getClass());
 
     @Override
     protected JndiRegistry createRegistry() throws Exception {
         JndiRegistry jndi = super.createRegistry();
-
         ds = new BasicDataSource();
-        ds.setDriverClassName("oracle.jdbc.OracleDriver");
-        ds.setUrl("jdbc:oracle:thin:@localhost:1521:ORCL");
-        ds.setUsername("flex");
-        ds.setPassword("Password1!");
+
+        ClassLoader classLoader = getClass().getClassLoader();
+        Properties p = new Properties();
+        p.load(new FileReader(new File(classLoader.getResource("oraevent.properties").getFile())));
+
+        ds.setDriverClassName(p.getProperty("jdbc.driverClassName"));
+        ds.setUrl(p.getProperty("jdbc.url"));
+        ds.setUsername(p.getProperty("jdbc.username"));
+        ds.setPassword(p.getProperty("jdbc.password"));
         ds.setAccessToUnderlyingConnectionAllowed(true);
 
         jdbc = new JdbcTemplate(ds);
-        jdbc.execute("DROP TABLE DEPT");
+        try {
+            jdbc.execute("DROP TABLE DEPT");
+        }catch(Exception ex){
+            log.info("DEPT table didn't exist");
+        }
         jdbc.execute("CREATE TABLE DEPT(DEPTNO NUMBER,DNAME VARCHAR2(50))");
 
-        //use jndi.bind to bind your beans
+        //bind the beans
         jndi.bind("ds", ds);
         jndi.bind("jdbcTemplate", jdbc);
         return jndi;
     }
+
 
     @Override
     protected RoutesBuilder createRouteBuilder() throws Exception {
@@ -68,10 +78,8 @@ public class IntegrationTest extends CamelTestSupport {
         };
     }
 
-
     @Test
     public void testEventFired() throws Exception {
-
         MockEndpoint me = (MockEndpoint) context.getEndpoint("mock:dbevent");
         me.expectedMessageCount(1);
         jdbc.execute("insert into dept (deptno,dname) values ('45','cool dept')");
